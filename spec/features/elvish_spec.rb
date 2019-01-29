e5039892db54c230abfe7d244443f8842157d339
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-01-29 08:12:48 +0000

    Test for button and translation box

---
+++ spec/features/elvish_spec.rb
@@ -0,0 +1,14 @@
+feature 'it should translate into Elvish' do
+
+  scenario 'it has a translate button' do
+    visit '/'
+    expect(page).to have_content "Welcome to the Elvish Translator"
+    expect(page).to have_button 'Translate'
+  end
+
+  scenario 'it has a box for text to translate' do
+    visit '/'
+    expect(page).to have_field 'Enter text to translate'
+  end
+
+end
